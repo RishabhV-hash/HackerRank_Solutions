b22392d365260388b9a5c2e02689f59ff83af380
Rishabh Verma <rishabhv11@gmail.com>
2020-09-25 06:11:47 +0530

    Project_Euler

---
+++ Project_Euler/Euler_5.py
@@ -0,0 +1,6 @@
+from math import gcd
+from functools import reduce
+
+for _ in range(int(input())):
+    N = int(input())
+    print(reduce(lambda x,y: x*y//gcd(x,y), range(1,N+1)))
---
+++ Project_Euler/Euler_6.py
@@ -0,0 +1,8 @@
+t=int(input())
+while t>0:
+    n=int(input())
+    j=n*(n+1)*(2*n+1)//6
+    m=n*(n+1)//2
+    m=m*m
+    print(m-j)
+    t -=1
